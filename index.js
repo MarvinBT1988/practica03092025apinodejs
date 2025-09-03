5d0b7f6310c1603ea9af8466451376bfe0b90f12
marvin.barrera.trigueros <marvin.barrera.trigueros@esfe.agape.edu.sv>
2025-09-03 18:11:42 +0000

    avance

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-
+app.use(express.json());
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -15,6 +15,23 @@ app.get('/', (req, res) => {
 
 app.get('/api', (req, res) => {
   res.json({"msg": "Hello world"});
+});
+
+app.post('/cliente', (req, res) => {
+  // Desestructurar el nombre y apellido del cuerpo de la solicitud (body)
+  const { nombre, apellido } = req.body;
+
+  // Validar si los campos están presentes
+  if (!nombre || !apellido) {
+    // Si falta alguno, enviar un error 400 (Bad Request)
+    return res.status(400).json({ error: 'Faltan el nombre o el apellido' });
+  }
+
+  // Concatenar el nombre y apellido para formar el nombre completo
+  const nombreCompleto = `${nombre} ${apellido}`;
+
+  // Enviar una respuesta JSON con el nombre completo
+  res.json({ nombreCompleto: nombreCompleto });
 });
 
 app.listen(port, () => {
